3cbdf1c97266565a6af2c22ee8c20d2b96c5a272
Алина <112811472+Alnpchn@users.noreply.github.com>
2022-09-19 22:53:09 +0300

    Add files via upload

---
+++ задание 1 лр 3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<locale.h>
+void main(void)
+{
+	setlocale(LC_ALL, "RUS");
+	int num, A, k;
+	puts("введите число", "\n");
+	scanf("%d", &num);
+	A = num;
+	puts("введено число A");
+	puts("введите число B");
+	scanf("%d", &k);
+	puts("введено число B");
+
+	printf("A+B=%=%d\n", k + num);
+	printf("A-B=%=%d\n", num - k);
+	printf("A*B=%=%d\n", k * num);
+	printf("частное B/A %=%d\n", k / num);
+	printf("остаток B/A=%=%d\n", k % num);
+}
